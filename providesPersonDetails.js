7eb1290f5c836242337ce46a9ef24d47fc10bc25
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-10-11 20:23:05 +0530

    gives personal details

---
+++ providesPersonDetails.js
@@ -0,0 +1,10 @@
+revKeysWithValues = require["./library_reverseKeysWithVAlues.js"].revKeys;
+
+const main = function(){
+  let PersonDetails = { 18 : ["a","b","c"], 19 : ["e" ,"f", "g"]};
+  console.log(generateAgePerNames(namesPerAge, "b"));
+  return;
+}
+
+main();
+
