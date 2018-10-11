494e162174b906983007daf4893b121b32ae93d8
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-10-11 20:32:46 +0530

    Deleted duplicate

---
+++ library_reverseKeysWithVAlues.js
@@ -0,0 +1,22 @@
+const assignAgeToNames = function(names, ageOfNames, agePerNames) {
+  for(let name of names){
+    agePerNames[name] = ageOfNames; 
+    }
+  return agePerNames ;
+}
+
+const generateAgePerNames = function(namesPerAge, nameToFind) {
+
+  let length = Object.keys(namesPerAge).length;
+  let agePerNames = {};
+
+  for(let i = 0; i < length; i++){
+    let ageOfNames =  Object.keys(namesPerAge)[i];
+    let names = Object.values(namesPerAge)[i];
+     agePerNames = assignAgeToNames(names, ageOfNames,agePerNames);
+  }
+
+  return agePerNames[nameToFind];
+}
+
+exports.revKeys = generateAgePerNames;
