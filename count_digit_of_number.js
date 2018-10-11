6bd736fd9051348f366289481675988805f4adc9
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-10-11 19:19:03 +0530

    counts digits of given number
    counter increments inside while till the remainder becomes equal to
    actual number, when divisor is multiplied by 10 evrytime the loop
    runs.

---
+++ count_digit_of_number.js
@@ -0,0 +1,30 @@
+let assert = require("assert").equal;
+
+const countDigits = function(number) {
+  let divisor = 10;
+  let counter = 0;
+  let remainder = 0;
+  while(number!=remainder){
+    remainder = number % divisor;
+    divisor*=10;
+    counter++;
+  }
+  return counter; 
+}
+
+const main = function(){
+  let inputNumber = +process.argv[2];
+  let expectedOutput = +process.argv[3];
+  let count = countDigits(inputNumber);
+  assert(count,expectedOutput);
+  console.log(count);
+  return;
+}
+
+main();
+
+
+
+
+
+
